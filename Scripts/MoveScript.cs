27b1285cd79e664725943bf6e09d61d0e95a89fa
Ross Squires <ross.squires@gmail.com>
2023-10-10 20:35:33 -0400

    Spider

--- Scripts/MoveScript.cs
+++ Scripts/MoveScript.cs
@@ -4,21 +4,21 @@ using Flecs.NET.Core;
 [GlobalClass, Icon("res://resources/tools/script.png"), Component]
 public partial class MoveScript : Node, Script
 {
-    public async Task Run(Entity entity)
-    {
-        while (true)
-        {
-            entity.Set(new MoveCommand { X = 200, Y = 200 });
-            await entity.OnRemoveAsync<MoveCommand>();
+	public async Task Run(Entity entity)
+	{
+		while (true)
+		{
+			entity.Set(new MoveCommand { X = 200, Y = 200 });
+			await entity.OnRemoveAsync<MoveCommand>();
 
-            entity.Set(new MoveCommand { X = 300, Y = 200 });
-            await entity.OnRemoveAsync<MoveCommand>();
+			entity.Set(new MoveCommand { X = 400, Y = 200 });
+			await entity.OnRemoveAsync<MoveCommand>();
 
-            entity.Set(new MoveCommand { X = 300, Y = 300 });
-            await entity.OnRemoveAsync<MoveCommand>();
+			entity.Set(new MoveCommand { X = 400, Y = 400 });
+			await entity.OnRemoveAsync<MoveCommand>();
 
-            entity.Set(new MoveCommand { X = 200, Y = 300 });
-            await entity.OnRemoveAsync<MoveCommand>();
-        }
-    }
+			entity.Set(new MoveCommand { X = 200, Y = 400 });
+			await entity.OnRemoveAsync<MoveCommand>();
+		}
+	}
 }
